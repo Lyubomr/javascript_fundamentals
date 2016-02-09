700d428f0ef6922ecb10ea1d74e30a48f25ce2c6
Lyubomr <s.lyubomyr@gmail.com>
2016-02-09 17:51:20 -0500

    Complete array assignment

--- js_fundamentals/arrays.js
+++ js_fundamentals/arrays.js
@@ -5,6 +5,7 @@
 // Create an array to hold your top choices (colors, presidents, whatever).
 // For each choice, log to the screen a string like: "My #1 choice is blue."
 // Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is.
-
-
-
+var myTopcars = [ "G65 AMG", "RS6 Avant", "V60 Polestar", "E63 AMG"]
+for (var i = 0; i < myTopCars.length; i++) { j = i+1
+  console.log("My" + " #" + j + " choice is " + myTopCars[i]);
+}
